7a060f4cfcf234c4d0be639f77dc1c9338cac7f7
ogawa_kazuhisa <ogawa_kazuhisa@cyberagent.co.jp>
2020-02-25 19:27:58 +0900

    勉強

---
+++ src/main/scala/Chapter1/Chapter1.scala
@@ -0,0 +1,30 @@
+package Chapter1
+
+object Chapter1 extends App {
+
+//  class Cafe {
+//    def buyCoffee(cc: CreditCard): (Coffee, Charge) = {
+//      val cup = new Coffee()
+//      (cup, Charge(cup.price))
+//    }
+//
+//    def buyCoffees(cc: CreditCard, n: Int): (List[Coffee], Charge) = {
+//      val purchases: List[(Coffee, Charge)] = List.fill(n)(buyCoffee(cc))
+//      val (coffees, charges) = purchases.unzip
+//      (coffees, charges.reduce((c1, c2) => c1.combine(c2)))
+//    }
+//  }
+//
+//  case class Charge(cc: CreditCard, amount: Double) {
+//    def combine(other: Charge): Charge =
+//      if (cc == other.cc)
+//        Charge(cc, amount + other.amount)
+//      else
+//        throw new Exception("Can't combine charges to different cards")
+//
+//    def coalesce(charges: List[Charge]): List[Charge] =
+//      charges.groupBy(_.cc).values.map(_.reduce(_ combine _)).toList
+//  }
+
+  println("Chapter1")
+}
