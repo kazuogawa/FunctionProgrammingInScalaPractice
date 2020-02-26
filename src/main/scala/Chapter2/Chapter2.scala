04454d8012129abd247cf7e6ceb03ec459b02555
kazuogawa <poppoppan04@gmail.com>
2020-02-27 00:52:19 +0900

    fib

--- src/main/scala/Chapter2/Chapter2.scala
+++ src/main/scala/Chapter2/Chapter2.scala
@@ -1,8 +1,15 @@
 package Chapter2
 
 object Chapter2 extends App {
-  def fib(n: Int): Int = ???
+  def fib(n: Int): Int = {
+    @scala.annotation.tailrec
+    def loop(key: Int, prev: Int, next: Int): Int =
+      if (n == key) prev else loop(key + 1, next, prev + next)
 
-  println(assert(fib(3) == 1))
-  println(assert(fib(3) == 5))
+    loop(0, 0, 1)
+  }
+
+  (1 to 10).foreach { i =>
+    println("fib %d : %d".format(i, fib(i)))
+  }
 }
