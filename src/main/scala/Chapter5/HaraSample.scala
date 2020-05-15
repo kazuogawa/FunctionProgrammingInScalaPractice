0e623044ed5b588b2f6bada955b358441f9d920b
ogawa_kazuhisa <ogawa_kazuhisa@cyberagent.co.jp>
2020-05-15 11:28:31 +0900

    原さんサンプル追加

---
+++ src/main/scala/Chapter5/HaraSample.scala
@@ -0,0 +1,51 @@
+package Chapter5
+
+object HaraSample {
+
+  trait Stream[+A] {
+    def take(n: Int): Stream[A] = this match {
+      case Cons(h, t) if n > 1 => Stream.cons(h(), t().take(n - 1))
+      case Cons(h, _) if n == 1 => Stream.cons(h(), Stream.empty)
+      case _ => Stream.empty
+    }
+
+    def toList: List[A] = {
+      @annotation.tailrec
+      def go(s: Stream[A], acc: List[A]): List[A] = s match {
+        case Cons(h, t) => go(t(), h() :: acc)
+        case _ => acc
+      }
+
+      go(this, List()).reverse
+    }
+  }
+
+  case object Empty extends Stream[Nothing]
+
+  case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+  object Stream {
+    def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+      lazy val head = hd
+      lazy val tail = tl
+      Cons(() => head, () => tail)
+    }
+
+    def empty[A]: Stream[A] = Empty
+
+    def apply[A](as: A*): Stream[A] =
+      if (as.isEmpty) empty
+      else cons(as.head, apply(as.tail: _*))
+
+  }
+
+  object StreamSample {
+    val ones: Stream[Int] = Stream.cons(1, ones)
+
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(StreamSample.ones.take(5).toList) // List(1, 1, 1, 1, 1)
+  }
+}
