043e7e5cc31b64aaa7940069f9618af8dea7348b
ogawa_kazuhisa <ogawa_kazuhisa@cyberagent.co.jp>
2020-06-03 14:26:01 +0900

    6.9まで

--- src/main/scala/Chapter6/Chapter6.scala
+++ src/main/scala/Chapter6/Chapter6.scala
@@ -71,12 +71,90 @@ object Chapter6 {
       @scala.annotation.tailrec
       def go(c: Int, l: List[Int], r: RNG): (List[Int], RNG) =
         if (c <= 0) (l, r) else {
-          val (i, nextRNG) = nonNegativeInt(r)
+          //整数(-も+も入る)なのでnegaはだめ
+          val (i, nextRNG) = r.nextInt
           go(c - 1, i :: l, nextRNG)
         }
 
       go(count, List(), rng)
     }
+
+    // RNGを使ってAを作成し、あとから別のアクションに利用できる新しい状態へRNGを遷移させる
+    type Rand[+A] = RNG => (A, RNG)
+    val int: Rand[Int] = _.nextInt
+
+    //RNGの状態を明示的にやり取りするのを回避したRandのActionを結合するためのコンビネータ
+    //ドメイン固有の言語のようなものになる
+    //unitは常に同じ定数とrngを返す
+    def unit[A](a: A): Rand[A] = rng => (a, rng)
+
+    def map[A, B](s: Rand[A])(f: A => B): Rand[B] = rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
+
+    def nonNegativeEven: Rand[Int] = map(nonNegativeInt)(i => i - i % 2)
+
+    //exercise 6.5
+    def doubleViaMap: Rand[Double] = map(nonNegativeInt)(i => i / (Int.MaxValue.toDouble + 1))
+
+    //exercise6.6
+    //Rand[Rand[C]]になるので、Randはずしたい.rngをどう合成するのか・・・
+    //def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    //  map(ra)(a => map(rb)(b => f(a, b)))
+    //答え。そっか、r1かr2だけとりだせばよかったのか
+    def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    //ここのrngって何？
+      rng => {
+        val (a, r1) = ra(rng)
+        val (b, r2) = rb(r1)
+        (f(a, b), r2)
+      }
+
+    def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] = map2(ra, rb)((_, _))
+
+    val randIntDouble: Rand[(Int, Double)] = both(int, double)
+    val randDoubleInt: Rand[(Double, Int)] = both(double, int)
+
+    //exercise 6.7
+    //答え見た
+    //foldRightか。。。unit使うのはなんでだろう・・・
+    def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+      fs.foldRight(unit(List[A]()))((f, acc) => map2(f, acc)(_ :: _))
+
+    //List.fill(n)(x)でlist作成できるらしい
+    //こたえみた。すげえ。こんな単純にできるのか
+    def intsViaSequence(count: Int): Rand[List[Int]] = sequence(List.fill(count)(int))
+
+    //0-nの整数を生成する
+    //Int.MaxValueはnで割り切れないことがあるため、再帰的に呼び出し
+    def nonNegativeLessThan(n: Int): Rand[Int] = { rng =>
+      val (i, rng2) = nonNegativeInt(rng)
+      val mod = i % n
+      //わざわざ符号反転しているかもしれないことをこういう表現で表すのはどうなのか・・・
+      if (i + (n - 1) - mod >= 0) (mod, rng2)
+      else nonNegativeLessThan(n)(rng)
+    }
+
+    //exercise 6.8
+    //答え見た。なんでこうなるの？根本をわかっていない可能性がある
+    def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = rng => {
+      val (a, r1) = f(rng)
+      g(a)(r1) // We pass the new state along
+    }
+    //答え見た。なんでこうなるの？根本をわかっていない可能性がある
+    def nonNegativeLessThanViaFlatMap(n: Int): Rand[Int] =
+      flatMap(nonNegativeInt) { i =>
+        val mod = i% n
+        if (i + (n-1) - mod >= 0) unit(mod) else nonNegativeLessThan(n)
+      }
+
+    //exercise 6.9
+    //全然わかっていないができた
+    def mapViaFlatMap[A, B](s: Rand[A])(f: A => B): Rand[B] = flatMap(s)(a => unit(f(a)))
+    def map2ViaFlatMap[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+      flatMap(ra)(a => mapViaFlatMap(rb)(b => f(a, b)))
+
   }
 
 
