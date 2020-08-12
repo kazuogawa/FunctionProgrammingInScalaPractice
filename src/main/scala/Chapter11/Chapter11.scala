65913ca4e4e27360c3c40e6b3f7d08accc79cc00
ogawa_kazuhisa <ogawa_kazuhisa@cyberagent.co.jp>
2020-08-12 14:50:01 +0900

    11章終わり

--- src/main/scala/Chapter11/Chapter11.scala
+++ src/main/scala/Chapter11/Chapter11.scala
@@ -82,6 +82,14 @@ object Chapter11 {
     //flatMap = flatten + mapなのでflattenするだけってことか
     def join[A](mma: F[F[A]]): F[A] = flatMap(mma)(ma => ma)
 
+    //exercise 11.13
+    //joinとmapでflatMapとcomposeをつくる
+    //型パズル？
+    def flatMapViaJoinAndMap[A, B](ma: F[A])(f: A => F[B]): F[B] = join(map(ma)(f))
+
+    def composeViaJoinAndMap[A, B, C](f: A => F[B], g: B => F[C]): A => F[C] =
+      a => join(map(f(a))(g))
+
   }
 
   //ふぁんくたはmapがある。
@@ -143,6 +151,124 @@ object Chapter11 {
   //exercise 11.3,4, 6,7,8はMonadの中
   //9はわからん
 
+  //exercise 11.17
+  //Idを使ってMonad[Id]を実装
+  case class Id[A](value: A) {
+    //そっか自分がvalue持ってるからmaいらないのか
+    //def map[A, B](ma: Id[A])(f: A => B): Id[B] = Id(f(ma.value))
+
+    //def flatMap[A, B](ma: Id[A])(f: A => Id[B]): Id[B] = f(ma.value)
+
+    def map[B](f: A => B): Id[B] = Id(f(value))
+
+    def flatMap[B](f: A => Id[B]): Id[B] = f(value)
+  }
+
+  val idMonad: Monad[Id] = new Monad[Id] {
+    override def unit[A](a: => A): Id[A] = Id(a)
+
+    @scala.annotation.tailrec
+    override def flatMap[A, B](ma: Id[A])(f: A => Id[B]): Id[B] = flatMap(ma)(f)
+  }
+
+  type IntState[A] = State[Int, A]
+
+  object IntStateMonad extends Monad[IntState] {
+    override def unit[A](a: => A): IntState[A] = State(s => (a, s))
+
+    override def flatMap[A, B](ma: IntState[A])(f: A => IntState[B]): IntState[B] = ma.flatMap(f)
+  }
+
+  //typeを分けて書かずにinlineで書くことも可能らしい
+  object IntStateMonadInline extends Monad[({type IntState[A] = State[Int, A]})#IntState] {
+    override def unit[A](a: => A): IntState[A] = State(s => (a, s))
+
+    override def flatMap[A, B](ma: IntState[A])(f: A => IntState[B]): IntState[B] = ma.flatMap(f)
+  }
+
+  def stateMonad[S]: Monad[({type f[x] = State[S, x]})#f] = new Monad[({type f[x] = State[S, x]})#f] {
+    override def unit[A](a: => A): State[S, A] = State(s => (a, s))
+
+    override def flatMap[A, B](ma: State[S, A])(f: A => State[S, B]): State[S, B] = ma.flatMap(f)
+
+
+  }
+
+  def getState[S]: State[S, S] = State(s => (s, s))
+
+  def setState[S](s: => S): State[S, Unit] = State(_ => ((), s))
+
+  /*
+  exercise 11.18 答え
+  `State`の` replicateM`は同じ状態遷移を何度も繰り返し、結果のリストを返します。
+  同じ開始状態を何度も渡すのではなく、1つの出力状態が次の入力状態になるように呼び出しをチェーンします。
+
+  `map2`は2つの状態遷移を取り、一方の出力状態をもう一方の入力に送るという点で同様に機能します。
+  出力はリストには入れられず、関数 `f`と組み合わされます。
+
+  `sequence`は状態遷移のリスト全体を受け取り、` replicateM`と同じ種類のことを行います。
+  最初の状態遷移の出力状態を次の状態遷移の入力状態にフィードします。 結果はリストに蓄積されます。
+   */
+
+  /*
+  //exercise 11.19 答え
+  Getting and setting the same state does nothing:
+  getState.flatMap(setState) == unit(())
+
+  written as for-comprehension:
+  for {
+    x <- getState
+    _ <- setState(x)
+  } yield ()
+
+  Setting the state to `s` and getting it back out yields `s`.
+  setState(s).flatMap(_ => getState) == unit(s)
+
+  alternatively:
+  for {
+    _ <- setState(s)
+    x <- getState
+  } yield x
+
+  */
+
+  val F = stateMonad[Int]
+
+  def zipWIthIndex[A](as: List[A]): List[(Int, A)] =
+    as.foldLeft(F.unit(List[(Int, A)]()))((acc, a) => for {
+      xs <- acc
+      n <- getState
+      _ <- setState(n + 1)
+    } yield (n, a) :: xs).run(0)._1.reverse
+
+  //モナドの規約は行間で何が起きるのかを指定するのではなく、何が起きようともそれが結合率と同一率を満たすことを明示するだけ
+  //exercise 11.20
+
+  /*
+   意味の説明
+   リーダーの `flatMap`のアクションは、` r`引数を外側のリーダーと内側のリーダーである `f`の結果の両方に渡すことです。
+   「状態」が状態を渡す方法に似ていますが、「リーダー」では「状態」が読み取り専用であることを除きます。
+
+   ここでの「シーケンス」の意味は、関数のリストがある場合、1つの引数を取り、
+   それをリスト内のすべての関数に渡し、結果のリストを返す関数に変換できることです。
+
+   `join`の意味は、両方の引数として同じ値をバイナリ関数に渡すことです。
+
+  `replicateM`の意味は、同じ関数を同じ引数に何度も適用し、結果のリストを返すことです。
+  この関数が_pure_（そうである必要があります）である場合、関数を何度も呼び出す代わりに、
+  関数を1回だけ適用して結果を複製することでこれを利用できます。
+  これは、ReaderモナドがreplicateMをオーバーライドして、非常に効率的な実装を提供できることを意味します。
+   */
+
+  case class Reader[R, A](run: R => A)
+
+  object Reader {
+    def readerMonad[R]: Monad[({type f[x] = Reader[R, x]})#f] = new Monad[({type f[x] = Reader[R, x]})#f] {
+      def unit[A](a: => A): Reader[R, A] = ???
+
+      def flatMap[A, B](st: Reader[R, A])(f: A => Reader[R, B]): Reader[R, B] = ???
+    }
+  }
 
   def main(args: Array[String]): Unit = {
     //exercise 11.5
@@ -173,6 +299,25 @@ object Chapter11 {
 
     //exercise 11.10, 11
     //証明は諦め・・・
+
+    //exercise 11.14
+    //join,map,unitの3つにのみ言及するようにモナド則を言い換えよ
+    //問題の意味がわからない。理解することを放棄
+    //15,16は飛ばす
+
+    //monadのインスタンスは以下のいずれかの実装を提供しなければならない
+    //unit, flatMap
+    //unit, compose
+    //unit, map, join
+    //モナドとは、結合率と同一率を満たす最小限のモナドコビネータの集まりのいずれかを実装したもの
+
+    //単位元monad
+    val id1: Id[String] = Id("Hello, ").flatMap(s1 => Id("monad!").flatMap(s2 => Id(s1 + s2)))
+    val id2: Id[String] = for {
+      a <- Id("Hello, ")
+      b <- Id("monad!")
+    } yield a + b
+    //モナドは変数を定義してバインドするためのコンテキストを提供し、変数置換を実行する
 
 
   }
