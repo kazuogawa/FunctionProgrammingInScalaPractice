7021f78e5b89a9f6a6d04b15f840afa01f0ef9cd
ogawa_kazuhisa <ogawa_kazuhisa@cyberagent.co.jp>
2020-09-09 14:56:24 +0900

    12.11まで

--- src/main/scala/Chapter12/Chapter12.scala
+++ src/main/scala/Chapter12/Chapter12.scala
@@ -30,7 +30,8 @@ object Chapter12 {
 
   trait Applicative[F[_]] extends Functor[F] {
     //プリミティブコンビネータ
-    def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C]
+    //map2ViaUnitAndApplyに実装したのでそれを使う
+    def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = map2ViaUnitAndApply(fa, fb)(f)
 
     def unit[A](a: => A): F[A]
 
@@ -86,52 +87,95 @@ object Chapter12 {
     def map4[A, B, C, D, E](fa: F[A], fb: F[B], fc: F[C], fd: F[D])(f: (A, B, C, D) => E): F[E] =
       apply(apply(apply(apply(unit(f.curried))(fa))(fb))(fc))(fd)
 
-    //flatMapに基づいてmap2のデフォルト実装を提供すればMonad[F]をApplicative[F]の部分型(subtype)にすることが可能
+    //exercise 12.8
+    //答え見た
+    def product[G[_]](G: Applicative[G]): Applicative[({type f[x] = (F[x], G[x])})#f] = {
+      val self = this
+      new Applicative[({type f[x] = (F[x], G[x])})#f] {
+        def unit[A](a: => A): (F[A], G[A]) = (self.unit(a), G.unit(a))
 
-    trait Monad[F[_]] extends Applicative[F] {
-      def join[A](ffa: F[F[A]]): F[A] = flatMap(ffa)(fa => fa)
+        override def apply[A, B](fs: (F[A => B], G[A => B]))(p: (F[A], G[A])): (F[B], G[B]) =
+          (self.apply(fs._1)(p._1), G.apply(fs._2)(p._2))
 
-      def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B] =
-        join(map(fa)(f))
-
-      def compose[A, B, C](f: A => F[B], g: B => F[C]): A => F[C] =
-        a => flatMap(f(a))(g)
-
-      override def map[A, B](fa: F[A])(f: A => B): F[B] =
-        flatMap(fa)(a => unit(f(a)))
-
-      def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
-        flatMap(fa)(a => map(fb)(b => f(a, b)))
+      }
     }
 
-    //今のところはAPIの関数を整理して型シグネチャにしたがって実装しているだけ・・・笑
+    //exercise 12.9
+    def compose[G[_]](G: Applicative[G]): Applicative[({type f[x] = F[G[x]]})#f] = {
+      val self = this
+      new Applicative[({type f[x] = F[G[x]]})#f] {
+        override def unit[A](a: => A): F[G[A]] = self.unit(G.unit(a))
 
-    //map2とunitの組み合わせはモナドの最小限の演算ではない。下記joinを実装することができない
-    //def join[A](f: F[F[A]]): F[A]
+        //答え見た。composeとproductで最低限実装すべき物が違うのか・・・
+        override def map2[A, B, C](fga: F[G[A]], fgb: F[G[B]])(f: (A, B) => C): F[G[C]] =
+          self.map2(fga, fgb)(G.map2(_, _)(f))
+      }
+    }
 
-    //ApplicativeとMonadの相違点
-    //Applicative: 計算の構造が固定,コンテキストに依存しない
-    //Monad: 前の作用の結果に基づいて構造を動的に選択できる, コンテキストに依存させることができる, 作用はファーストクラス(事前に選択されるのではなく解釈時に生成できる
+    //exercise 12.10
+    //このってどれ？上二つとも？
+    //理解するのを放棄した
 
-    //じゃあなんでApplicativeがあるの？重要な点は？
-    //アプリカティブファンク他は合成されるが、モナドは合成されない
+    //exercise 12.11
+    //monadでcomposeを記述できないことを試す
+    def composeViaMonad[G[_]](G: Monad[G]): Monad[({type f[x] = F[G[x]]})#f] = {
+      val self = this
+      new Monad[({type f[x] = F[G[x]]})#f] {
+        override def unit[A](a: => A): F[G[A]] = self.unit(G.unit(a))
 
-    //streamのapplicativeはmap2, unitは定義できるがflatMapは定義できない
-
-    val streamApplicative: Applicative[Stream] = new Applicative[Stream] {
-      override def map2[A, B, C](fa: Stream[A], fb: Stream[B])(f: (A, B) => C): Stream[C] =
-        fa.zip(fb).map(f.tupled)
-
-      //無限の定数ストリーム
-      override def unit[A](a: => A): Stream[A] = Stream.continually(a)
-
-      //exercise 12.4
-      //sequenceにどのような意味があるか
-      //Listにまとめられる？ってこと？
-      override def sequence[A](a: List[Stream[A]]): Stream[List[A]] = super.sequence(a)
+        //できないらしい
+        //override def flatMap[A, B](fa: F[G[A]])(f: A => F[G[B]]): F[G[B]] =
+        // self.flatMap(na => G.flatMap(na)(a => ???))
+      }
     }
   }
 
+
+  //flatMapに基づいてmap2のデフォルト実装を提供すればMonad[F]をApplicative[F]の部分型(subtype)にすることが可能
+
+  trait Monad[F[_]] extends Applicative[F] {
+    def join[A](ffa: F[F[A]]): F[A] = flatMap(ffa)(fa => fa)
+
+    def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B] =
+      join(map(fa)(f))
+
+    def compose[A, B, C](f: A => F[B], g: B => F[C]): A => F[C] =
+      a => flatMap(f(a))(g)
+
+    override def map[A, B](fa: F[A])(f: A => B): F[B] =
+      flatMap(fa)(a => unit(f(a)))
+
+    def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
+      flatMap(fa)(a => map(fb)(b => f(a, b)))
+
+  }
+
+  //今のところはAPIの関数を整理して型シグネチャにしたがって実装しているだけ・・・笑
+
+  //map2とunitの組み合わせはモナドの最小限の演算ではない。下記joinを実装することができない
+  //def join[A](f: F[F[A]]): F[A]
+
+  //ApplicativeとMonadの相違点
+  //Applicative: 計算の構造が固定,コンテキストに依存しない
+  //Monad: 前の作用の結果に基づいて構造を動的に選択できる, コンテキストに依存させることができる, 作用はファーストクラス(事前に選択されるのではなく解釈時に生成できる
+
+  //じゃあなんでApplicativeがあるの？重要な点は？
+  //アプリカティブファンク他は合成されるが、モナドは合成されない
+
+  //streamのapplicativeはmap2, unitは定義できるがflatMapは定義できない
+
+  val streamApplicative: Applicative[Stream] = new Applicative[Stream] {
+    override def map2[A, B, C](fa: Stream[A], fb: Stream[B])(f: (A, B) => C): Stream[C] =
+      fa.zip(fb).map(f.tupled)
+
+    //無限の定数ストリーム
+    override def unit[A](a: => A): Stream[A] = Stream.continually(a)
+
+    //exercise 12.4
+    //sequenceにどのような意味があるか
+    //Listにまとめられる？ってこと？
+    override def sequence[A](a: List[Stream[A]]): Stream[List[A]] = super.sequence(a)
+  }
   //exercise 12.5
 
   def eitherMonad[E]: Monad[({type f[x] = Either[E, x]})#f] = new Monad[({type f[x] = Either[E, x]})#f] {
@@ -238,20 +282,19 @@ object Chapter12 {
   //上記の法則がなかればどっちから関数を当てるかをわけるmap3L,map3Rのようなことをしないといけない。
   //def product[A, B](fa: F[A], F[B]): F[A, B] =
   //  map2(fa,fb)((_, _))
-  def assoc[A, B, C](p: (A, (B, C)): ((A, B), C) =
+  def assoc[A, B, C](p: (A, (B, C))): ((A, B), C) =
     p match {
       case (a, (b, c)) => ((a, b), c)
-      case _ => _
     }
 
   //productコンビネータとassocコンビネータをを使用した場合、アプリカティブファンクタの結合則は下記になる
   //左は左結合、右は右結合になっているのに注意
   //product(product(fa, fb), fc) == map(product(fa, product(fb, fc)))(assoc)
-
   val F: Applicative[Option] = new Applicative[Option] {
-    override def map2[A, B, C](fa: Option[A], fb: Option[B])(f: (A, B) => C): Option[C] = ???
+    override def map2[A, B, C](fa: Option[A], fb: Option[B])(f: (A, B) => C): Option[C] =
+      fa.flatMap(a => fb.map(b => f(a, b)))
 
-    override def unit[A](a: => A): Option[A] = ???
+    override def unit[A](a: => A): Option[A] = Some(a)
   }
 
   case class Employee(name: String, id: Int)
@@ -263,12 +306,46 @@ object Chapter12 {
       s"${e.name} makes ${pay.rate * pay.hoursPerYear}"
     }
 
-  val e: Option[Employee] = ???
-  val pay: Option[Pay] = ???
-  format(e, pay)
+  //val e: Option[Employee] = ???
+  //val pay: Option[Pay] = ???
+  //format(e, pay)
 
-  def format(e: Option[String], pay: Option[Double]): Option[String] = {
+  def formatStringDouble(e: Option[String], pay: Option[Double]): Option[String] = {
     F.map2(e, pay) { (e, pay) => s"$e makes $pay" }
   }
+
+  //自然性の法則
+  //map2(a, b)(productF(f,g)) == product(map(a)(f), map(b)(g))
+  //2つの関数を1つの関数に結合し、両方の引数を受け取り、結果をペアで返す
+  def productF[I, O, I2, O2](f: I => O, g: I2 => O2): (I, I2) => (O, O2) =
+    (i, i2) => (f(i), g(i2))
+
+  //アプリカティブの法則はunit, map, map2の動作に一貫性と合理性があることを保証する
+
+  //exercise 12.7
+  //全てのモナドがアプリカティブファンクタであることを証明せよ
+  //答え見た
+  //map2(unit(), fa)((_, a) => a) == fa
+  //map2(fa, unit())((_, a) => a) == fa
+  //map2の拡張
+  //def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => F[C]) =
+  //  flatMap(fa)(a => map(fb)(b => f(a, b)))
+
+  // unit()と(_, a) => aを代入
+  //  flatMap(unit())(u => map(fa)(a => a)) == fa
+  // a => aは単にfaなので
+  //  flatMap(unit())(u => fa) == fa
+  // 最初の関数の引数として「Unit」を使用することにより、「flatMap」は「compose」を使用して書き換えることができることを思い出してください。
+  // compose(unit, u => fa)(()) => fa
+  // 簡略可能なので
+  // (u => fa)(()) == fa
+  // fa == fa
+  // まとめるとこんなかんじ
+  //    flatMap(fa)(a => map(unit(()))(u => a)) == fa
+  //    flatMap(fa)(a => unit(a)) == fa  // via functor laws
+  //    compose(u => fa, unit)(()) == fa
+  //    (u => fa)(()) == fa
+  //    fa == fa
+
 
 }
